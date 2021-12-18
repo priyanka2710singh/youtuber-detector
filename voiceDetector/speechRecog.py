2d9f46e598a2389b8fcf1fd448eaff182da02dc4
priyanka2710singh <priyanka2724singh@gmail.com>
2021-12-18 16:31:08 +0530

    added speech recognition to open webcam

---
+++ voiceDetector/speechRecog.py
@@ -0,0 +1,86 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import time
+import cv2
+
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+# if not necessary then delete this func
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+def openCamera():
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
+
+    while (True):
+
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+    time.sleep(3)
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak...")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+wishMe()
+
+if __name__=='__main__':
+
+
+    while True:
+        # speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+
+        if statement == 0:
+            continue
+
+        if 'open camera' in statement:
+            openCamera()
+            break
+
+time.sleep(3)
+
